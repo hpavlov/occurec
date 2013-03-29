9ca75cdbf38b56b64914d6cb98ff39d2fd802bc6
Hristo Pavlov <hristo_dpavlov@yahoo.com>
2013-03-30 09:06:22 +1100

    Replaced the old ASCOM icon with the new one

--- AAVRec/frmRunAction.Designer.cs
+++ AAVRec/frmRunAction.Designer.cs
@@ -28,90 +28,90 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRunAction));
-			this.label1 = new System.Windows.Forms.Label();
-			this.lblActionToRun = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.tbxParamValue = new System.Windows.Forms.TextBox();
-			this.btnExecute = new System.Windows.Forms.Button();
-			this.btnCancel = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(30, 25);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(40, 13);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Action:";
-			// 
-			// lblActionToRun
-			// 
-			this.lblActionToRun.AutoSize = true;
-			this.lblActionToRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.lblActionToRun.Location = new System.Drawing.Point(76, 25);
-			this.lblActionToRun.Name = "lblActionToRun";
-			this.lblActionToRun.Size = new System.Drawing.Size(89, 13);
-			this.lblActionToRun.TabIndex = 1;
-			this.lblActionToRun.Text = "Action To Run";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 51);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(58, 13);
-			this.label2.TabIndex = 2;
-			this.label2.Text = "Parameter:";
-			// 
-			// tbxParamValue
-			// 
-			this.tbxParamValue.Location = new System.Drawing.Point(79, 48);
-			this.tbxParamValue.Name = "tbxParamValue";
-			this.tbxParamValue.Size = new System.Drawing.Size(265, 20);
-			this.tbxParamValue.TabIndex = 3;
-			// 
-			// btnExecute
-			// 
-			this.btnExecute.DialogResult = System.Windows.Forms.DialogResult.Yes;
-			this.btnExecute.Location = new System.Drawing.Point(188, 96);
-			this.btnExecute.Name = "btnExecute";
-			this.btnExecute.Size = new System.Drawing.Size(75, 23);
-			this.btnExecute.TabIndex = 4;
-			this.btnExecute.Text = "Execute";
-			this.btnExecute.UseVisualStyleBackColor = true;
-			// 
-			// btnCancel
-			// 
-			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnCancel.Location = new System.Drawing.Point(269, 96);
-			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.Size = new System.Drawing.Size(75, 23);
-			this.btnCancel.TabIndex = 5;
-			this.btnCancel.Text = "Cancel";
-			this.btnCancel.UseVisualStyleBackColor = true;
-			// 
-			// frmRunAction
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(371, 135);
-			this.Controls.Add(this.btnCancel);
-			this.Controls.Add(this.btnExecute);
-			this.Controls.Add(this.tbxParamValue);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.lblActionToRun);
-			this.Controls.Add(this.label1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "frmRunAction";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "Run Action";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRunAction));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblActionToRun = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxParamValue = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Action:";
+            // 
+            // lblActionToRun
+            // 
+            this.lblActionToRun.AutoSize = true;
+            this.lblActionToRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblActionToRun.Location = new System.Drawing.Point(76, 25);
+            this.lblActionToRun.Name = "lblActionToRun";
+            this.lblActionToRun.Size = new System.Drawing.Size(89, 13);
+            this.lblActionToRun.TabIndex = 1;
+            this.lblActionToRun.Text = "Action To Run";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Parameter:";
+            // 
+            // tbxParamValue
+            // 
+            this.tbxParamValue.Location = new System.Drawing.Point(79, 48);
+            this.tbxParamValue.Name = "tbxParamValue";
+            this.tbxParamValue.Size = new System.Drawing.Size(265, 20);
+            this.tbxParamValue.TabIndex = 3;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.btnExecute.Location = new System.Drawing.Point(188, 96);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 4;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(269, 96);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmRunAction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 135);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.tbxParamValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblActionToRun);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmRunAction";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Run Action";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
--- AAVRec/frmUnexpectedError.Designer.cs
+++ AAVRec/frmUnexpectedError.Designer.cs
@@ -28,44 +28,44 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUnexpectedError));
-			this.lblError = new System.Windows.Forms.Label();
-			this.btnClose = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// lblError
-			// 
-			this.lblError.Location = new System.Drawing.Point(12, 20);
-			this.lblError.Name = "lblError";
-			this.lblError.Size = new System.Drawing.Size(331, 134);
-			this.lblError.TabIndex = 0;
-			// 
-			// btnClose
-			// 
-			this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnClose.Location = new System.Drawing.Point(134, 170);
-			this.btnClose.Name = "btnClose";
-			this.btnClose.Size = new System.Drawing.Size(75, 23);
-			this.btnClose.TabIndex = 1;
-			this.btnClose.Text = "&Close";
-			this.btnClose.UseVisualStyleBackColor = true;
-			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-			// 
-			// frmUnexpectedError
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(355, 205);
-			this.Controls.Add(this.btnClose);
-			this.Controls.Add(this.lblError);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "frmUnexpectedError";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "Error";
-			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmUnexpectedError_FormClosing);
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUnexpectedError));
+            this.lblError = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblError
+            // 
+            this.lblError.Location = new System.Drawing.Point(12, 20);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(331, 134);
+            this.lblError.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(134, 170);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmUnexpectedError
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 205);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblError);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmUnexpectedError";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Error";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmUnexpectedError_FormClosing);
+            this.ResumeLayout(false);
 
 		}
 
