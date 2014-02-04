08b32b520458ad172c988567b88d3147a738110f
Hristo Pavlov <hristo_dpavlov@yahoo.com>
2014-02-04 17:56:12 +1100

    Added WAT-910BD test project

---
+++ WAT910BD.Tester/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WAT910BD.Tester
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.serialPort = new System.IO.Ports.SerialPort(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.cbxCOMPort = new System.Windows.Forms.ComboBox();
+			this.btnConnect = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(53, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "COM Port";
+			// 
+			// cbxCOMPort
+			// 
+			this.cbxCOMPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbxCOMPort.FormattingEnabled = true;
+			this.cbxCOMPort.Location = new System.Drawing.Point(69, 8);
+			this.cbxCOMPort.Name = "cbxCOMPort";
+			this.cbxCOMPort.Size = new System.Drawing.Size(98, 21);
+			this.cbxCOMPort.TabIndex = 1;
+			// 
+			// btnConnect
+			// 
+			this.btnConnect.Location = new System.Drawing.Point(174, 7);
+			this.btnConnect.Name = "btnConnect";
+			this.btnConnect.Size = new System.Drawing.Size(75, 23);
+			this.btnConnect.TabIndex = 2;
+			this.btnConnect.Text = "Connect";
+			this.btnConnect.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+			this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.textBox1.Location = new System.Drawing.Point(0, 307);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(543, 138);
+			this.textBox1.TabIndex = 3;
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(543, 445);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.btnConnect);
+			this.Controls.Add(this.cbxCOMPort);
+			this.Controls.Add(this.label1);
+			this.Name = "frmMain";
+			this.Text = "WAT-910BD Control Tester";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.IO.Ports.SerialPort serialPort;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox cbxCOMPort;
+		private System.Windows.Forms.Button btnConnect;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
