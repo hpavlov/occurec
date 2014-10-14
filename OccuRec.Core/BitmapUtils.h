7965bc392a2035f151f755b4ab69ecf2ab35b693
Hristo Pavlov <hristo_dpavlov@yahoo.com>
2014-10-15 08:48:28 +1100

    Added MPLv2 license headers to OccuRec.Core.

--- OccuRec.Core/BitmapUtils.h
+++ OccuRec.Core/BitmapUtils.h
@@ -1,21 +1,7 @@
-// The following ifdef block is the standard way of creating macros which make exporting 
-// from a DLL simpler. All files within this DLL are compiled with the KOYASHVIDEOUTILS_EXPORTS
-// symbol defined on the command line. This symbol should not be defined on any project
-//tabs=4
-// --------------------------------------------------------------------------------
-//
-// Koyash.VideoUtilities - Unmanaged implementation
-//
-// Description:	Header file 
-//
-// Author:		(HDP) Hristo Pavlov <hristo_dpavlov@yahoo.com>
-//
-// --------------------------------------------------------------------------------
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// that uses this DLL. This way any other project whose source files include this file see 
-// KOYASHVIDEOUTILS_API functions as being imported from a DLL, whereas this DLL sees symbols
-// defined with this macro as being exported.
 #ifdef KOYASHVIDEOUTILS_EXPORTS
 #define KOYASHVIDEOUTILS_API __declspec(dllexport)
 #else
--- OccuRec.Core/OccuRec.Core.h
+++ OccuRec.Core/OccuRec.Core.h
@@ -1,3 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
 // The following ifdef block is the standard way of creating macros which make exporting 
 // from a DLL simpler. All files within this DLL are compiled with the OCCURECCORE_EXPORTS
 // symbol defined on the command line. This symbol should not be defined on any project
