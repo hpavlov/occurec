6c23d3cbf7828af251797d1f8a5fea00a3ef59dc
Hristo Pavlov <hristo_dpavlov@yahoo.com>
2019-11-05 00:23:04 +0000

    white space formatting for prettifying the file.

--- OccuRec.Core/cpuid.cpp
+++ OccuRec.Core/cpuid.cpp
@@ -1,6 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // Code based on https://software.intel.com/en-us/articles/using-cpuid-to-detect-the-presence-of-sse-41-and-sse-42-instruction-sets/
 // Intel specs for CPU feaures: https://www.intel.com/content/www/us/en/architecture-and-technology/64-ia-32-architectures-software-developer-vol-2a-manual.html
@@ -12,50 +12,50 @@
 
 int isRDTSCSupported (void)
 {
-	CPUIDinfo Info;
+    CPUIDinfo Info;
     int rVal = 0;
-	if (isCPUIDsupported())
+    if (isCPUIDsupported())
     {
-		// CPUID.01H:EDX[4]
+        // CPUID.01H:EDX[4]
         get_cpuid_info(&Info, 0x1, 0x0);
-		if ((Info.EDX & 0x10) == 0x10)
+        if ((Info.EDX & 0x10) == 0x10)
         {
             rVal = 1;
         }
-	}
-	return rVal;
+    }
+    return rVal;
 }
 
 int isRDTSCPSupported (void)
 {
-	CPUIDinfo Info;
+    CPUIDinfo Info;
     int rVal = 0;
-	if (isCPUIDsupported())
+    if (isCPUIDsupported())
     {
-		// CPUID.80000001H:EDX[27]
+        // CPUID.80000001H:EDX[27]
         get_cpuid_info(&Info, 0x80000001, 0x0);
-		if ((Info.EDX & 0x08000000) == 0x08000000)
+        if ((Info.EDX & 0x08000000) == 0x08000000)
         {
             rVal = 1;
         }
-	}
-	return rVal;
+    }
+    return rVal;
 }
 
 int isInvariantTSCAvailable (void)
 {
-	CPUIDinfo Info;
+    CPUIDinfo Info;
     int rVal = 0;
-	if (isCPUIDsupported())
+    if (isCPUIDsupported())
     {
-		// CPUID.80000007H:EDX[8]
+        // CPUID.80000007H:EDX[8]
         get_cpuid_info(&Info, 0x80000007, 0x0);
-		if ((Info.EDX & 0x100) == 0x100)
+        if ((Info.EDX & 0x100) == 0x100)
         {
             rVal = 1;
         }
-	}
-	return rVal;
+    }
+    return rVal;
 }
 
 int cpuid_checked = 0;
@@ -68,31 +68,31 @@ int isCPUIDsupportedInternal (void)
     __asm 
     {
         push ecx ; save ecx
-        pushfd ; push original EFLAGS
-        pop eax ; get original EFLAGS
-        mov ecx, eax ; save original EFLAGS
-        xor eax, 200000h ; flip bit 21 in EFLAGS
-        push eax ; save new EFLAGS value on stack
-        popfd ; replace current EFLAGS value
-        pushfd ; get new EFLAGS
-        pop eax ; store new EFLAGS in EAX
-        xor eax, ecx ; Bit 21 of flags at 200000h will be 1 if CPUID exists
-        shr eax, 21  ; Shift bit 21 bit 0 and return it
-        push ecx
-        popfd ; restore bit 21 in EFLAGS first
-        pop ecx ; restore ecx
+            pushfd ; push original EFLAGS
+            pop eax ; get original EFLAGS
+            mov ecx, eax ; save original EFLAGS
+            xor eax, 200000h ; flip bit 21 in EFLAGS
+            push eax ; save new EFLAGS value on stack
+            popfd ; replace current EFLAGS value
+            pushfd ; get new EFLAGS
+            pop eax ; store new EFLAGS in EAX
+            xor eax, ecx ; Bit 21 of flags at 200000h will be 1 if CPUID exists
+            shr eax, 21  ; Shift bit 21 bit 0 and return it
+            push ecx
+            popfd ; restore bit 21 in EFLAGS first
+            pop ecx ; restore ecx
     }
 }
 
 int isCPUIDsupported (void)
 {
-	if (cpuid_checked == 0)
-	{
-		cpuid_supported = isCPUIDsupportedInternal();
-		cpuid_checked = 1;
-	}
+    if (cpuid_checked == 0)
+    {
+        cpuid_supported = isCPUIDsupportedInternal();
+        cpuid_checked = 1;
+    }
 
-	return cpuid_supported;
+    return cpuid_supported;
 }
 
 void get_cpuid_info (CPUIDinfo *Info, const unsigned int leaf, const unsigned int subleaf)
@@ -104,18 +104,18 @@ void get_cpuid_info (CPUIDinfo *Info, const unsigned int leaf, const unsigned in
     __asm 
     {
         mov edx, Info   ; addr of start of output array
-        mov eax, leaf  ; leaf
-        mov ecx, subleaf  ; subleaf
-        push edi
-        push ebx
-        mov  edi, edx                      ; edi has output addr
-        cpuid
-        mov DWORD PTR [edi], eax
-        mov DWORD PTR [edi+4], ebx
-        mov DWORD PTR [edi+8], ecx
-        mov DWORD PTR [edi+12], edx
-        pop ebx
-        pop edi
+            mov eax, leaf  ; leaf
+            mov ecx, subleaf  ; subleaf
+            push edi
+            push ebx
+            mov  edi, edx                      ; edi has output addr
+            cpuid
+            mov DWORD PTR [edi], eax
+            mov DWORD PTR [edi+4], ebx
+            mov DWORD PTR [edi+8], ecx
+            mov DWORD PTR [edi+12], edx
+            pop ebx
+            pop edi
     }
 }
 
